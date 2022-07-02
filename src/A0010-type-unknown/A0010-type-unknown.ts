c684b4c757a23b90102d452924e08f96b3db346b
zBiggui <bigguisenna@gmail.com>
2022-07-02 16:45:04 -0300

    add A0010-type-unknown.ts

---
+++ src/A0010-type-unknown/A0010-type-unknown.ts
@@ -0,0 +1,11 @@
+// safer than 'any', because it needs to check the type previously
+
+let x: unknown;
+
+x = 100;
+x = 'Luiz';
+x = 900;
+x = 10;
+const y = 800;
+
+if (typeof x === 'number') console.log(x + y);
