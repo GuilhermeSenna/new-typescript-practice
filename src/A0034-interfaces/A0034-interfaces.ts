9e8d50f8bf5b0537e6f66e904b736d6da1524f72
zBiggui <bigguisenna@gmail.com>
2022-07-17 21:41:43 -0300

    feat: add A0034

---
+++ src/A0034-interfaces/A0034-interfaces.ts
@@ -0,0 +1,43 @@
+// type TipoPessoa = {
+//     nome: string;
+//     sobrenome: string;
+//     nomeCompleto(): string;
+// };
+
+interface TipoNome {
+    nome: string;
+}
+
+interface TipoSobrenome {
+    sobrenome: string;
+}
+
+interface TipoNomeCompleto {
+    nomeCompleto(): string;
+}
+
+// Only with type alias
+// interface TipoPessoa = TipoNome & TipoSobrenome & TipoNomeCompleto;
+
+interface TipoPessoa2 extends TipoNome, TipoSobrenome, TipoNomeCompleto { }
+
+export class Pessoa implements TipoPessoa2 {
+    // protected nome: string;
+    // protected sobrenome: string;
+    constructor(public nome: string, public sobrenome: string) { }
+
+    nomeCompleto(): string {
+        return this.nome + ' ' + this.sobrenome;
+    }
+}
+
+const pessoaObj: TipoPessoa2 = {
+    nomeCompleto() {
+        return this.nome + ' ' + this.sobrenome;
+    },
+    nome: 'Luiz',
+    sobrenome: 'Agora está OK',
+}
+
+const pessoa = new Pessoa('Guilherme', 'Senna');
+console.log(pessoa.nomeCompleto());
