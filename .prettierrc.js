837163fb431ff925f0e99518f8ea9b9d5485c829
zBiggui <bigguisenna@gmail.com>
2022-07-21 23:42:53 -0300

    build: change identing configs

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
     semi: true,
     trailingComma: 'all',
     singleQuote: true,
-    printwidth: 80,
-    tabWidth: 4,
-    endOfLine: 'auto',
+    printWidth: 80,
+    tabWidth: 2,
+    bracketSpacing: false,
 }
