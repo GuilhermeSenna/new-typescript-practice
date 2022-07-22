2529fcfa11d0ea619c64672fc58771c40a1d467f
zBiggui <bigguisenna@gmail.com>
2022-07-21 22:37:03 -0300

    feat: add A0038-keyof-typeof

---
+++ src/A0038-keyof-typeof/A0038-keyof-typeof.ts
@@ -0,0 +1,24 @@
+// type CoresObj = {
+//     vermelho: string;
+//     verde: string;
+//     azul: string;
+// };
+
+type CoresObj = typeof coresObj;
+type CoresChaves = keyof CoresObj;
+
+const coresObj = {
+    vermelho: 'red',
+    verde: 'green',
+    azul: 'blue',
+    roxo: 'purple',
+};
+
+function traduzirCor(cor: CoresChaves, cores: CoresObj) {
+    return cores[cor];
+}
+
+console.log(traduzirCor('vermelho', coresObj));
+console.log(traduzirCor('verde', coresObj));
+console.log(traduzirCor('azul', coresObj));
+console.log(traduzirCor('roxo', coresObj));
