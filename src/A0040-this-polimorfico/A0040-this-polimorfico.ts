be124cb617b6b071fc02b76d22affd4a8b6d7664
zBiggui <bigguisenna@gmail.com>
2022-07-23 01:14:52 -0300

    feat: add A0040

---
+++ src/A0040-this-polimorfico/A0040-this-polimorfico.ts
@@ -0,0 +1,59 @@
+export class Calculadora {
+    constructor(public numero: number) { }
+
+    add(n: number): this {
+        this.numero += n;
+        return this;
+    }
+
+    sub(n: number): this {
+        this.numero -= n;
+        return this;
+    }
+
+    div(n: number): this {
+        this.numero /= n;
+        return this;
+    }
+
+    mul(n: number): this {
+        this.numero *= n;
+        return this;
+    }
+}
+
+export class SubCalculadora extends Calculadora {
+    pow(n: number): this {
+        this.numero **= n;
+        return this;
+    }
+}
+
+const calculadora = new SubCalculadora(10);
+calculadora.add(5).mul(2);
+console.log(calculadora);
+
+// Builder - GoF
+
+export class RequestBuilder {
+    private method: 'get' | 'post' | null = null;
+    private url: string | null = null;
+
+    setMethod(method: 'get' | 'post'): this {
+        this.method = method;
+        return this;
+    }
+
+    setUrl(url: string): this {
+        this.url = url;
+        return this;
+    }
+
+    send(): void {
+        console.log(`Enviando dados via ${this.method} para ${this.url}`);
+    }
+}
+
+const request = new RequestBuilder();
+request.setUrl('http://www.google.com');
+request.setMethod('post').send();
