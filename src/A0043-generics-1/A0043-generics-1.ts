ce6a0e9240e8d372c5d80c2006e35c83e4f836f8
zBiggui <bigguisenna@gmail.com>
2022-07-31 19:28:32 -0300

    feat: add A0043

---
+++ src/A0043-generics-1/A0043-generics-1.ts
@@ -0,0 +1,28 @@
+// Custom function
+type filterCallback<U> = (value: U, index?: number, array?: U[]) => boolean;
+
+export const meuFilter = <T>(
+    array: T[],
+    callbackfn: filterCallback<T>,
+): T[] => {
+    const novoArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (callbackfn(array[i])) {
+            novoArray.push(array[i]);
+        }
+    }
+
+    return novoArray;
+};
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const arrayFiltradoOriginal = array.filter((value) => value < 5);
+console.log(arrayFiltradoOriginal);
+
+const arrayFiltrado = meuFilter(array, (value) => {
+    if (typeof value === 'number') return value < 5;
+    return false;
+});
+console.log(arrayFiltradoOriginal);
