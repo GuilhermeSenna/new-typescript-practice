54178afb8831a5eadf9d67365a436242ce4b097e
zBiggui <bigguisenna@gmail.com>
2022-08-11 23:18:12 -0300

    feat: add A0062

---
+++ src/A0062-external-libs/A0062-external-libs.ts
@@ -0,0 +1,6 @@
+// when install a lib, install the lib and '@types/lib -D'
+import validator from 'validator';
+import _ from 'lodash';
+
+console.log(validator.isEmail('guilhermesenna_16@hotmail.com'));
+console.log(_.clone([1, 2, 3, 4, 5]));
