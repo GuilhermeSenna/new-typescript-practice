ac4ff6ad13778e57007f650e84d924daf13e7d4c
zBiggui <bigguisenna@gmail.com>
2022-08-11 21:52:40 -0300

    feat: finish A0060 logic

--- src/A0060-namespaces/index.ts
+++ src/A0060-namespaces/index.ts
@@ -1,23 +1,4 @@
-/* eslint-disable @typescript-eslint/no-namespace */
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference path="module/module.ts" />
 
-namespace MeuNamespace {
-    export const nomeDoNameSpace = 'Luiz';
-
-    export class PessoaDoNamespace {
-        constructor(public nome: string) {}
-    }
-
-    const pessoaDoNameSpace = new PessoaDoNamespace('Guilherme');
-    console.log(pessoaDoNameSpace);
-
-    export namespace OutroNamespace {
-        export const nomeDoNamespace = 'Nome do outro namespace';
-    }
-}
-
-const pessoaDoNameSpace = new MeuNamespace.PessoaDoNamespace('Guilherme');
-console.log(pessoaDoNameSpace);
 console.log(MeuNamespace.nomeDoNameSpace);
-console.log(MeuNamespace.OutroNamespace.nomeDoNamespace);
-
-export default 1;
